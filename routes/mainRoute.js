e0d3845ad64902150ab09294f2ef3adcbf92131e
Supakorn <supakorn2807@protonmail.com>
2022-03-21 17:27:54 +0700

    Update mainRoute.js

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,7 +1,7 @@
 const main = (req, res) => {
   res.json({
     Github: "H3X-T",
-    Repo: "https://github.com/H3X-T/projects-api",
+    Repo: "https://github.com/H3X-T/upload-api",
     Facebook: "Supakorn Ieamgomol",
     Instagram: "supakornigm"
   });
